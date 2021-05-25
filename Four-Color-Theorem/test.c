d5cec4a24c41767b5c464f7cbea4ab023a34b3d0
CharlieWang0422 <charlie930422@gmail.com>
2021-05-25 17:30:39 +0800

    xx

---
+++ Four-Color-Theorem/test.c
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int i,j,x;
+int color_save;
+int color_variety = 1;
+int table[30][30];
+int colors[100] = {0};
+int country_relationship[100][100]={0};
+bool gooutloop = true;
+vector<vector<int>> S;
+vector<int>country_color(100,96);
+
+
+ // DFS function
+void DFS(int pos){
+	bye:
+	if(gooutloop){
+	//end condition 
+		if(pos == 100){
+			gooutloop = false;
+		  	for( x = 1;x < 100;x++){
+		  		colors[x] = country_color[x];
+	 		}
+	 		goto bye;
+		}
+		// If the colors are not enough
+		else if(pos == 0){
+			color_variety++;
+			pos = 1;
+			S.clear();
+			DFS(pos);
+		}
+		// try to fill the color
+		else if(pos != 100){
+			for(i=1;i<=color_variety;i++){ 
+				color_save = 96+i;
+				country_color[pos] = color_save;
+				// make sure this way has not tried before
+				for(unsigned int k = 0;k<S.size();k++){
+					if(S[k] == country_color){
+						country_color[pos] = 96;
+						// go to place
+						goto place;
+					}
+				}
+				// Confirm that the neighbors are the same
+				for(j = 1;j <= 100;j++){
+					if(country_relationship[pos][j] == 1){
+						if(country_color[j] == color_save && j != pos){
+							country_color[pos] = 96;
+							// go to place
+							goto place;
+							}
+						}
+
+				}
+				// record this way
+				S.push_back(country_color);
+				// go to next country
+				DFS(pos+1);
+				// if the color can not accord,come to this place
+				place:
+				// Check if the colors are all used up
+				if(i == color_variety){
+					country_color[pos-1] = 96;
+					DFS(pos-1);
+					}	
+				}
+			}
+	}
+	// DFS finish
+	else{
+		return;
+	}
+}	
+	// main function
+int main(){
+	// scan the file
+    freopen("color900_1.txt","r",stdin);
+    for(i=0; i<30; i++) {
+    	for(j=0; j<30; j++) {
+    		scanf("%d", &table[i][j]);
+
+    		} 
+    }
+    // check neigbor relationship
+    for(i=0;i<30;i++){
+    	for(j=0;j<30;j++){	
+    		if(i!=29){
+    			country_relationship[table[i][j]][table[i+1][j]] = 1;
+    		}
+    		if(i!= 0){
+    			country_relationship[table[i][j]][table[i-1][j]] = 1;
+    		}
+    		if(j!=29){
+    			country_relationship[table[i][j]][table[i][j+1]] = 1;
+    		}
+    		if(j!=0){
+    			country_relationship[table[i][j]][table[i][j-1]] = 1;
+    		}
+    	}
+    }
+    DFS(1);
+    // fill color
+	 for(i=0;i<30;i++){
+    	for(j=0;j<30;j++){	
+    		table[i][j] = colors[table[i][j]];
+    	}
+    }
+    // print out the result
+    for(i = 0; i < 30; i++) {
+    	for(j = 0; j < 30; j++) {
+    		if(table[i][j] == 0){
+    			if(j==29){
+    				cout << 0 <<"\n";
+    			}
+    			else{
+    				cout << 0 << " ";
+    			}
+    		}
+    		else{
+    			if (j == 29) {
+    			cout << char(table[i][j]) << "\n";
+    			} 
+    			else {
+    			cout << char(table[i][j]) << " ";
+    			}
+    		}
+    		
+		}
+	}
+}
