192fee0c7f9e37cbc53ddc535486eed4910b8374
CharlieWang0422 <charlie930422@gmail.com>
2021-05-27 22:29:38 +0800

    xx

--- Four-Color-Theorem/test.c
+++ Four-Color-Theorem/test.c
@@ -9,22 +9,15 @@ int color_variety = 1;
 int table[30][30];
 int colors[100] = {0};
 int country_relationship[100][100]={0};
-bool gooutloop = true;
 vector<vector<int>> S;
 vector<int>country_color(100,96);
 
 
  // DFS function
 void DFS(int pos){
-	bye:
-	if(gooutloop){
 	//end condition 
 		if(pos == 100){
-			gooutloop = false;
-		  	for( x = 1;x < 100;x++){
-		  		colors[x] = country_color[x];
-	 		}
-	 		goto bye;
+		  	return;
 		}
 		// If the colors are not enough
 		else if(pos == 0){
@@ -34,7 +27,7 @@ void DFS(int pos){
 			DFS(pos);
 		}
 		// try to fill the color
-		else if(pos != 100){
+		else if(pos!=100){
 			for(i=1;i<=color_variety;i++){ 
 				color_save = 96+i;
 				country_color[pos] = color_save;
@@ -61,25 +54,24 @@ void DFS(int pos){
 				S.push_back(country_color);
 				// go to next country
 				DFS(pos+1);
+				return;
 				// if the color can not accord,come to this place
 				place:
 				// Check if the colors are all used up
 				if(i == color_variety){
 					country_color[pos-1] = 96;
 					DFS(pos-1);
+					return; 
 					}	
 				}
 			}
-	}
 	// DFS finish
-	else{
-		return;
-	}
 }	
 	// main function
 int main(){
 	// scan the file
     freopen("color900_1.txt","r",stdin);
+    freopen("color900_1_out.txt","w",stdout);
     for(i=0; i<30; i++) {
     	for(j=0; j<30; j++) {
     		scanf("%d", &table[i][j]);
@@ -107,29 +99,19 @@ int main(){
     // fill color
 	 for(i=0;i<30;i++){
     	for(j=0;j<30;j++){	
-    		table[i][j] = colors[table[i][j]];
+    		table[i][j] = country_color[table[i][j]];
     	}
     }
     // print out the result
     for(i = 0; i < 30; i++) {
     	for(j = 0; j < 30; j++) {
-    		if(table[i][j] == 0){
-    			if(j==29){
-    				cout << 0 <<"\n";
-    			}
-    			else{
-    				cout << 0 << " ";
-    			}
-    		}
-    		else{
-    			if (j == 29) {
+    		if (j == 29) {
     			cout << char(table[i][j]) << "\n";
-    			} 
-    			else {
+    		} 
+    		else {
     			cout << char(table[i][j]) << " ";
-    			}
     		}
-    		
 		}
 	}
+	printf("The minimum number of color(s) needed is %d",color_variety );
 }
