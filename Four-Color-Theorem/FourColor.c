ed3f96692b10dd89de05cefa44b8bf5fb8b5ab69
CharlieWang0422 <charlie930422@gmail.com>
2021-06-12 21:15:17 +0800

    xx

--- Four-Color-Theorem/FourColor.c
+++ Four-Color-Theorem/FourColor.c
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 #include<unordered_set>
 
-
 using namespace std;
 
 
@@ -9,18 +8,15 @@ int i,j;
 int color_save;
 int color_variety = 1;
 int table[30][30];
-unsigned int numofcountry;
-int country_relationship[100][100]={0};
-// ,color_group[195][100] = {0};
-vector<vector<int>> S,group;
-vector<int>country_color(100,96),country_sequence,color_group;
+vector<vector<int>> country_relationship(100,vector<int>(100,0)) , country_relationship2, S;
+vector<int>country_color(100,96),country_sequence;
 
 
 
  // DFS function
-void DFS(int pos){
+void DFS(unsigned int pos){
 	//end condition 
-		if(pos == 100){
+		if(pos == country_sequence.size()){
 		  	return;
 		}
 		// If the colors are not enough
@@ -31,39 +27,39 @@ void DFS(int pos){
 			DFS(pos);
 		}
 		// try to fill the color
-		else if(pos!=100){
+		else if(pos!=country_sequence.size()+1){
 			for(i=1;i<=color_variety;i++){ 
 				color_save = 96+i;
-				country_color[pos] = color_save;
+				country_color[country_sequence[pos]] = color_save;
 				// make sure this way has not tried before
 				for(unsigned int k = 0;k<S.size();k++){
 					if(S[k] == country_color){
-						country_color[pos] = 96;
+						country_color[country_sequence[pos]] = 96;
 						// go to place
 						goto place;
 					}
 				}
 				// Confirm that the neighbors are the same
-				for(j = 1;j <= 100;j++){
-					if(country_relationship[pos][j] == 1){
-						if(country_color[j] == color_save && j != pos){
-							country_color[pos] = 96;
+				for(unsigned int j = 1;j < country_sequence.size();j++){
+					if(country_relationship2[pos][country_sequence[j]] == 1){
+						if(country_color[country_sequence[j]] == color_save && country_sequence[j] != country_sequence[pos]){
+							country_color[country_sequence[pos]] = 96;
 							// go to place
 							goto place;
 							}
 						}
-
 				}
 				// record this way
 				S.push_back(country_color);
 				// go to next country
+                // cout << pos;
 				DFS(pos+1);
 				return;
 				// if the color can not accord,come to this place
 				place:
 				// Check if the colors are all used up
 				if(i == color_variety){
-					country_color[pos-1] = 96;
+					country_color[country_sequence[pos-1]] = 96;
 					DFS(pos-1);
 					return; 
 					}	
@@ -82,10 +78,11 @@ int main(){
 
     		} 
     }
+    country_sequence.push_back(0);
     // check neigbor relationship
     for(i=0;i<30;i++){
     	for(j=0;j<30;j++){
-    		// record appear country
+    		// record appear country sequence
     		if(table[i][j] !=0){
     			for(unsigned int x = 0; x < country_sequence.size();x++){
     				if(country_sequence[x] == table[i][j]){
@@ -109,38 +106,15 @@ int main(){
     		}
     	}
     }
+    // rearrange the sequence in the order of appearance of the country
+    country_relationship2.push_back(country_relationship[0]);
+    for(unsigned int k = 1 ; k < country_sequence.size() ; k++){
+        country_relationship2.push_back(country_relationship[country_sequence[k]]);
+    }
+    
     DFS(1);
-    // change color sequence
-    for(unsigned int x=0 ; x < country_sequence.size() ; x++){
-    	for(auto &c:color_group){
-    		if(country_color[country_sequence[x]] == c){
-    			goto place2;
-    		}
-    	}
-    	color_group.push_back(country_color[country_sequence[x]]);
-    	place2:
-    	continue;
-    }
-    for(i = 0;i<color_variety;i++){
-    	group.push_back({});
-    	group[i].push_back(color_group[i]);
-    }
-    for(unsigned int x = 0;x < country_sequence.size();x++){
-    	for(i = 0 ;  i < color_variety ; i++){
-    		if(country_color[country_sequence[x]] == group[i][0]){
-    			group[i].push_back(country_sequence[x]);
-    		}
-    	}
-    }
-    for(i = 0 ; i < color_variety ; i++){
-    	for(unsigned int x =1 ; x<group[i].size() ; x++){
-    		country_color[group[i][x]] = 97 + i;
-    	}
-    }
- 	// for(auto &c : color_group){
-  //   	cout << c<<"\n";
-  //   }
-	 for(i=0;i<30;i++){
+    // write the color to the table
+	for(i=0;i<30;i++){
     	for(j=0;j<30;j++){	
     		table[i][j] = country_color[table[i][j]];
     	}
